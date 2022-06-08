6ba9c2867ffd16fee30b626264e4b1df4be7a2b2
Christopher Marzulli <chriszulli15@gmail.com>
2022-06-08 14:37:13 -0400

    commit tree d1b6cd192dc4d9cdf9cb1d3e48112f18ad4a0492

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,14 @@ function Home() {
   );
 }
 
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
+
 {/* write an <About> component here */}
 
 
@@ -23,8 +31,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
